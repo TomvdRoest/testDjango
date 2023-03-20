32d6e9cee839a02143cae55331223e5126a823af
TomvdRoest <tomvanderroest@hotmail.com>
2023-03-20 20:23:14 +0100

    Aanpassing invullen van de publish tijd

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -24,6 +24,10 @@ class PostFootballer(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
